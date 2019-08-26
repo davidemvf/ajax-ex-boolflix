2a9c1c3b51242558bd223d8f4fed7edb461602d7
davidemvf <davidemv.fiore@gmail.com>
2019-08-26 22:41:28 +0200

    Aggiunta funzione per il voto con le stelle.

---
+++ main.js
@@ -0,0 +1,60 @@
+$(document ).ready(function() {
+  console.log("ciao");
+
+  $("#go").click(function(){
+    // pulisco l'input
+    // $("#search").val("");
+    // salvo in una variabile il valore dell'input
+    var query = $("#search").val();
+
+    $.ajax(
+      {
+        url: "https://api.themoviedb.org/3/search/movie",
+        method: "GET",
+        data: {
+          api_key : "d063f38fe3e1f45729834c95133aff40",
+          language : "it-IT",
+          query : query
+        },
+        success: function(data) {
+
+          var movies = data.results;
+          console.log(movies);
+          for (var i = 0; i<movies.length; i++) {
+            // salvo l'elemento i-esimo in una variabile
+            var film = movies[i];
+            var source   = document.getElementById("entry-template").innerHTML;
+            var template = Handlebars.compile(source);
+            var context = {
+              titolo: film.title,
+              titolooriginale: film.original_title,
+              lingua: film.original_language,
+              voto: ratewithStars(film.vote_average)
+            };
+            var html = template(context);
+            $(".results").append(html);
+          }
+        },
+        error: function() {
+          alert("Errore");
+        }
+      }
+    )
+    // funzione per sostituire il voto con le stelle
+    function ratewithStars(rate) {
+      var rateRounded = Math.floor(rate/2);
+      var stars = "";
+      for (var i = 0; i < 5; i++) {
+        if (i < rateRounded) {
+          stars += '<i class="fas fa-star"></i>'
+        } else {
+          stars += '<i class="far fa-star"></i>'
+        }
+      }
+
+      return(stars)
+
+
+    }
+  })
+});
